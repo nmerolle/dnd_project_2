ffa6abd7bf0e8f4ec7f1d6c8ec585b98e87c47d8
Nicholas Merolle <nmerolle11@hotmail.com>
2020-12-31 01:24:01 +0000

    added environment files

--- lib/dnd5e_class_spells_2.rb
+++
@@ -1,6 +0,0 @@
-require "dnd5e_class_spells_2/version"
-
-module Dnd5eClassSpells2
-  class Error < StandardError; end
-  # Your code goes here...
-end
---
+++ lib/environment.rb
@@ -0,0 +1,8 @@
+require "pry"
+
+require_relative "./dnd5e_class_spells_2/version"
+require_relative "./dnd5e_class_spells_2/cli.rb"
+require_relative "./dnd5e_class_spells_2/api.rb"
+require_relative "./dnd5e_class_spells_2/spell.rb"
+require_relative "./dnd5e_class_spells_2/klass.rb"
+
